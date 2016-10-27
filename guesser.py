961c5bbf35d3bcf9c175318f5a727e507f8aece1
ThatGuyAgis <agisprivate@gmail.com>
2016-10-27 17:08:31 +0200

    trying to add "keep playing" option

--- guesser.py
+++ guesser.py
@@ -7,23 +7,40 @@ import random
 
 def main():
     print "Guess a number between 1 and 100."
+
+
+
     randomNumber = random.randint(1, 100)
+    userScore = 0
+
+
     found = False
+    carryOnPlaying = False
+
+    while not carryOnPlaying:
+        while not found :
+            userGuess = input("Your Guess: ")
 
 
-    while not found :
-        userGuess = input("Your Guess: ")
+            if userGuess == randomNumber:
+                print "You got it!"
+                found = True
+                userScore += 1
+            elif userGuess > randomNumber:
+                print "Guess lower!"
+            else:
+                print "Guess higher!"
 
-
-        if userGuess == randomNumber:
-            print "You got it!"
-            found = True
-        elif userGuess > randomNumber:
-            print "Guess lower!"
+        print "Your score: %s" % userScore
+        print " "
+        print " "
+        print " "
+        print "Do you want to carry on Playing?"
+        userAnswer = input("Your Answer: ")
+        if userAnswer == True:
+            carryOnPlaying = False
         else:
-            print "Guess higher!"
-    print "Thanks for playing!"
-
+            carryOnPlaying = True
 
 
 if __name__ == "__main__":
